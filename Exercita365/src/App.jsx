bccd8d52026e6cfde723a5bb73581eee25033665
juniorpinhodev <juniorpinhodev@gmail.com>
2024-05-01 19:00:35 -0300

    feat: 🎸 outlet em app.jsx

--- Exercita365/src/App.jsx
+++ Exercita365/src/App.jsx
@@ -1,46 +1,16 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { UsersContext } from './context/UsersContext'
+import { UsersContextProvider } from './context/UsersContext.jsx'
 import './App.css'
-
+import { Outlet } from 'react-router-dom'
 
 function App() {
 
-  const { users, registerUsers, editUsers, removeUsers } = useContext(UsersContext)
-
-  const [ newUsers, setNewUsers ] = useState({
-    nome: '',
-    email: '',
-    senha: ''
-  })
-
-    return (
-    <div>
-     <h1>Exercita365</h1>
-    
-      {!!users && users.map(user => (
-        <>
-        <h3 key={user.id}>{user.nome}</h3>
-        </>
-      ))}
-
-      <input type='text' 
-             placeholder='Digiter o nome do usuário'
-             value={newUsers.nome}
-             onChange={(e)=> setNewUsers({...newUsers, nome: e.target.value})}
-      />
-
-      <input type='email' 
-             placeholder='Digiter o email'
-             value={newUsers.email}
-             onChange={(e)=> setNewUsers({...newUsers, email: e.target.value})}
-      />
-      <input type='password' 
-             placeholder='Digiter a senha'
-             value={newUsers.senha}
-             onChange={(e)=> setNewUsers({...newUsers, senha: e.target.value})}
-      />
-             <button onClick={()=> registerUsers(newUsers)}>Acessar</button>
-    </div>
+  return (
+    <>
+      <UsersContextProvider>
+        <h1>Exercita 365</h1>
+        <Outlet />
+      </UsersContextProvider>
+    </>
   )
 }
 
