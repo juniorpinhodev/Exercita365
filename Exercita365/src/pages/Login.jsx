c96583ee3f27afbe30b05f1bd8d8360d2142fe82
juniorpinhodev <juniorpinhodev@gmail.com>
2024-05-01 19:14:55 -0300

    feat: 🎸 update

--- Exercita365/src/pages/Login.jsx
+++ Exercita365/src/pages/Login.jsx
@@ -1,21 +1,21 @@
 
 function Login (){
-    return(
-        <div className="container">
-            <h1>Faça o seu Login para acessar</h1>
+  return(
+      <div className="container">
+          <h1>Faça o seu Login para acessar</h1>
 
 
-            <input type="email" 
-                   placeholder="Digite o seu email"
-            />
+          <input type="email" 
+                 placeholder="Digite o seu email"
+          />
 
-            <input type="password"
-                   placeholder="Digite a sua senha"
-            />
-                    
-            <button onClick={()=> doLogin}>Acessar</button>
-        </div>
-    )
+          <input type="password"
+                 placeholder="Digite a sua senha"
+          />
+                  
+          <button onClick={()=> doLogin}>Acessar</button>
+      </div>
+  ) 
 }
 
 export default Login
