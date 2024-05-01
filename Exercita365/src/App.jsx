4b029f79ee6d8de9064549c15c79fc9c9b276d8b
juniorpinhodev <juniorpinhodev@gmail.com>
2024-05-01 16:18:12 -0300

    feat: 🎸 desenvolvimento do App

--- Exercita365/src/App.jsx
+++ Exercita365/src/App.jsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { UsersContext } from './context/UsersContext'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from '../src/pages/Login';
+
 
 function App() {
 
-  return (
+  const { users, registerUsers, editUsers, removeUsers } = useContext(UsersContext)
+
+  const [ newUsers, setNewUsers ] = useState({
+    nome: '',
+    email: '',
+    senha: ''
+  })
+
+    return (
     <div>
      <h1>Exercita365</h1>
-     <Login />
-     <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
+    
+      {!!users && users.map(user => (
+        <>
+        <h3 key={user.id}>{user.nome}</h3>
+        </>
+      ))}
+
+      <input type='text' 
+             placeholder='Digiter o nome do usuário'
+             value={newUsers.nome}
+             onChange={(e)=> setNewUsers({...newUsers, nome: e.target.value})}
+      />
+
+      <input type='email' 
+             placeholder='Digiter o email'
+             value={newUsers.email}
+             onChange={(e)=> setNewUsers({...newUsers, email: e.target.value})}
+      />
+      <input type='password' 
+             placeholder='Digiter a senha'
+             value={newUsers.senha}
+             onChange={(e)=> setNewUsers({...newUsers, senha: e.target.value})}
+      />
+             <button onClick={()=> registerUsers(newUsers)}>Acessar</button>
     </div>
   )
 }
