a56e93e2cb6a3e1014a26142fd5463929c0fe96b
juniorpinhodev <juniorpinhodev@gmail.com>
2024-05-01 18:36:22 -0300

    feat: 🎸 ajustes na main e packjson

--- Exercita365/src/main.jsx
+++ Exercita365/src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import UsersRegistration from './pages/UsersRegistration.jsx'
+
+
+const routes = createBrowserRouter([
+     {
+          path: '/',
+          element: <App />,
+          children: [
+               {
+                    path: '/users/registration',
+                    element: <UsersRegistration />
+               }
+          ]
+     }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+     <RouterProvider router={routes}>    
+     </RouterProvider>
 )
