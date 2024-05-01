6d889a3b1cbd422820e60f9a8d3a670dc438b7ac
juniorpinhodev <juniorpinhodev@gmail.com>
2024-05-01 19:21:51 -0300

    feat: 🎸 UserContext login

--- Exercita365/src/pages/Login.jsx
+++ Exercita365/src/pages/Login.jsx
@@ -1,21 +1,40 @@
-
-function Login (){
-  return(
-      <div className="container">
-          <h1>Faça o seu Login para acessar</h1>
+import React, { useContext} from 'react';
+import { UsersContext } from './context/UsersContext'
 
 
-          <input type="email" 
-                 placeholder="Digite o seu email"
-          />
+function Login() {
 
-          <input type="password"
-                 placeholder="Digite a sua senha"
-          />
-                  
-          <button onClick={()=> doLogin}>Acessar</button>
-      </div>
-  ) 
+  const { users } = useContext(UsersContext)
+
+
+    return (
+    <div>
+       
+      {!!users && users.map(user => (
+        <>
+        <h3 key={user.id}>{user.nome}</h3>
+        </>
+      ))}
+
+      <input type='text' 
+             placeholder='Digiter o nome do usuário'
+             value={newUsers.nome}
+             onChange={(e)=> setNewUsers({...newUsers, nome: e.target.value})}
+      />
+
+      <input type='email' 
+             placeholder='Digiter o email'
+             value={newUsers.email}
+             onChange={(e)=> setNewUsers({...newUsers, email: e.target.value})}
+      />
+      <input type='password' 
+             placeholder='Digiter a senha'
+             value={newUsers.senha}
+             onChange={(e)=> setNewUsers({...newUsers, senha: e.target.value})}
+      />
+             <button onClick={()=> registerUsers(newUsers)}>Acessar</button>
+    </div>
+  )
 }
 
 export default Login
