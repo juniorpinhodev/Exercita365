7c070a4f26e7f64ceb78b6c850e1223ac82e994d
juniorpinhodev <juniorpinhodev@gmail.com>
2024-05-01 21:46:00 -0300

    feat: 🎸 rotas dashboard list

--- Exercita365/src/router/routes.jsx
+++ Exercita365/src/router/routes.jsx
@@ -1,6 +1,11 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Routes, Route } from "react-router-dom";
 import App from '../App';
 import Login from '../pages/Login';
+import UsersRegistration from '../pages/UsersRegistration';
+import Dashboard from '../pages/Dashboard';
+import List from '../pages/List';
+import SearchCEP from "../pages/SearchCEP";
+import PrivateRoute from "../components/PrivateRoute";
 
 
 const routes = createBrowserRouter([
@@ -12,6 +17,31 @@ const routes = createBrowserRouter([
        path: "/",
        element: <Login />
       },
+      {
+        path: "/login",
+        element: <Login />
+      },
+      {
+        path: "/users/registration",
+        element: <UsersRegistration />
+      }, 
+      {
+        path: "/",
+        element: (
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        )
+       },
+       {
+        path: "/list/:id",
+        element: <List />
+       },
+       {
+        path: "/cep",
+        element: <SearchCEP />
+       }
+
      ]
     }
    ]);
