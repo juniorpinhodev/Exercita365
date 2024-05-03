6ae050e097cd1e017e6097dd91e57a82c9a61bae
juniorpinhodev <juniorpinhodev@gmail.com>
2024-05-03 11:45:04 -0300

    feat: 🎸 exibição no dashboard  de todos os locais cadastrados

--- Exercita365/src/pages/Dashboard.jsx
+++ Exercita365/src/pages/Dashboard.jsx
@@ -1,16 +1,41 @@
-
-
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 
 function Dashboard() {
+  const [locais, setLocais] = useState([]);
+
+  useEffect(() => {
+    const fetchLocais = async () => {
+      try {
+        // Substitua pelo código de busca dos locais no seu arquivo db.json
+        const response = await fetch('data/db.json');
+        const data = await response.json();
+        setLocais(data.list);
+      } catch (error) {
+        console.error('Erro ao buscar locais:', error);
+      }
+    };
+
+    fetchLocais();
+  }, []);
+
     return ( 
-     
-        <>
-        <h1>Dashboard</h1> 
-  
-      </>
+      <div>
+        <h1>Dashboard</h1>
         
+        <h2>Locais Cadastrados:</h2>
+      <ul>
+        {locais.map(local => (
+          <li key={local.id}>
+            {local.nome} - {local.descricao}
+          </li>
+        ))}
+      </ul>
+         
+        <Link to="/local-registration">Registrar Novo Local</Link>
       
+    </div>      
     );
   }
   
